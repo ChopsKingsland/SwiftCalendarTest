cdc9c506565b5f4e828fc1e948cdb5694cfd39ff
Charlie Kingsland <charliekingsland@outlook.com>
2021-03-28 10:47:30 +0100

    Initial Commit

---
+++ calendar/calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarApp.swift
+//  calendar
+//
+//  Created by Charlie Kingsland on 28/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
